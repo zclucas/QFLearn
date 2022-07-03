1384e6a3fa305c2a458a3a6b635a628a0f933fd1
zclucas <2660681757@qq.com>
2022-07-03 23:36:42 +0800

    之前

---
+++ Assets/Scenes/Game/Scripts/Event/GameStartEvent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public  class GameStartEvent:Event<GameStartEvent>
+{
+    // Start is called before the first frame update
+   
+}
---
+++ Assets/Scenes/Game/Scripts/Event/KillOneEvent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOneEvent : Event<KillOneEvent>
+{
+    // Start is called before the first frame update
+}
