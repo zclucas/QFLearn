53296f3be0a0ab86c766e6af330f728b211692ad
ZhangChao <zhangc@zhongbosoft.com>
2022-07-05 09:46:25 +0800

    ignore文件生效

--- Assets/Scenes/Game/Scripts/Event/GameStartEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-public  class GameStartEvent:Event<GameStartEvent>
-{
-    // Start is called before the first frame update
-   
-}
--- Assets/Scenes/Game/Scripts/Event/KillOneEvent.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class KillOneEvent : Event<KillOneEvent>
-{
-    // Start is called before the first frame update
-}
